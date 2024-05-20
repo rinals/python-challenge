0b8b59fa4ab562ead0411f0d9cc74da3689dd5b4
Rinal Shastri <rinal.shastri@outlook.com>
2024-05-20 15:59:30 -0700

    pybank total months

---
+++ PyBank/main.py
@@ -0,0 +1,17 @@
+import csv
+
+budget_file_path = "PyBank/Resources/budget_data.csv"
+with open(budget_file_path, "r") as file:
+    csv_reader = csv.DictReader(file)
+
+    rows_done = 0
+
+    total_months = 0
+
+    for row in csv_reader:
+        total_months += 1
+    
+
+    print("Financial Analysis")
+    print("----------------------------")
+    print(f"Total Months: {total_months}")
