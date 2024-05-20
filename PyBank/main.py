ff616fd9b22bf1ea73d44d0f68bd794e3195585d
Rinal Shastri <rinal.shastri@outlook.com>
2024-05-20 16:13:52 -0700

    Average change

--- PyBank/main.py
+++ PyBank/main.py
@@ -7,11 +7,29 @@ with open(budget_file_path, "r") as file:
     rows_done = 0
 
     total_months = 0
+    total = 0
+    previous_profit = 0
+    total_change = 0
 
     for row in csv_reader:
         total_months += 1
+        current_profit = int(row['Profit/Losses'])
+        total += current_profit
+
+        if rows_done > 0:
+            change_in_profit = current_profit - previous_profit
+            total_change += change_in_profit
+
+
+        rows_done += 1
+        previous_profit = current_profit
+
+    average_change = total_change / (rows_done - 1)
+    average_change = round(average_change, 2)
     
 
     print("Financial Analysis")
     print("----------------------------")
     print(f"Total Months: {total_months}")
+    print(f"Total: ${total}")
+    print(f"Average Change: ${average_change}")
